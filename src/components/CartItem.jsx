8431961589b8a3634710c688f6936d817801875e
Aman Kumar <amankumar0950888@gmail.com>
2025-08-08 14:57:53 +0530

    grid for cart responsivness

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { FcDeleteDatabase } from "react-icons/fc";
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import { remove } from '../redux/slices/CartSlice';
 
 function CartItem({ item }) {
     const dispatch = useDispatch();
+
     const removeFromCart = () => {
         dispatch(remove(item.id));
-    }
+    };
 
     return (
         <div className="w-full">
-            <div className='flex flex-col md:flex-row gap-6 px-4 md:px-10 py-6'>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 px-4 md:px-10 py-6">
 
                 {/* Image */}
-                <div className='w-full md:w-[150px] h-[180px] mx-auto md:mx-0'>
+                <div className="w-full h-[180px] md:col-span-1 mx-auto">
                     <img
                         src={
                             import.meta.env.MODE === "development"
@@ -22,25 +23,25 @@ function CartItem({ item }) {
                                 : item.image
                         }
                         alt="img"
-                        className='h-full w-full'
+                        className="h-full w-full object-contain"
                     />
                 </div>
 
                 {/* Text and Price Section */}
-                <div className='flex-1'>
-                    <h1 className='text-gray-700 font-semibold text-lg mb-3'>
-                        {item.title}
-                    </h1>
-                    <p className="text-sm text-gray-600">
-                        {item.description.split(" ").slice(0, 20).join(" ") + " ..."}
-                    </p>
+                <div className="md:col-span-3 flex flex-col justify-between">
+                    <div>
+                        <h1 className="text-gray-700 font-semibold text-lg mb-3">
+                            {item.title}
+                        </h1>
+                        <p className="text-sm text-gray-600">
+                            {item.description.split(" ").slice(0, 20).join(" ") + " ..."}
+                        </p>
+                    </div>
 
-                    <div className='flex justify-between items-center mt-5'>
-                        <p className='font-bold text-green-600 text-lg'>${item.price}</p>
-                        <div className='bg-red-200 p-1 rounded-full cursor-pointer'>
-                            <FcDeleteDatabase className='size-5'
-                                onClick={removeFromCart}
-                            />
+                    <div className="flex justify-between items-center mt-5">
+                        <p className="font-bold text-green-600 text-lg">${item.price}</p>
+                        <div className="bg-red-200 p-1 rounded-full cursor-pointer">
+                            <FcDeleteDatabase className="size-5" onClick={removeFromCart} />
                         </div>
                     </div>
                 </div>
